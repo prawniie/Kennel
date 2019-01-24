c1e090e3528c0f402f5cdcde3d7fcb0234e99963
georgfalemark <georg@falemark.se>
2019-01-24 15:35:05 +0100

    hekfsdks

--- Yoshis kennel/Yoshis kennel/wwwroot/contact-us/contact-us.js
+++ Yoshis kennel/Yoshis kennel/wwwroot/contact-us/contact-us.js
@@ -6,10 +6,57 @@ function getId(id) {
 
 let formSelect = getId("FormControlSelect1");
 let message = getId("FormControlTextarea1");
-let preName = getId("InputPrename")
-let lastName = getId("InputLastname")
-let telephoneNumber = getId("InputTelephoneNumber")
-let email = getId("InputEmail")
+
+
+let lastName = getId("InputLastname");
+
+let telephoneNumber = getId("InputTelephoneNumber");
+let email = getId("InputEmail");
+let submit = getId("submit");
+
+function validatePreName() {
+    let preName = getId("InputPrename");
+    if (preName == "") {
+        alert("Du måste fylla i ett namn");
+        return false;
+    }
+}
+
+
+document.getElementById("submit").addEventListener("click", validatePreName);
+
+
+
+
+//function respondToUser() {
+
+//    let enteredNumber = document.getElementById("yourAge").value;
+//    let theValueEnteredIsAnumber = enteredNumber == parseInt(enteredNumber);
+//    let response = "";
+//    if (theValueEnteredIsAnumber == true) {
+//        if (enteredNumber >= 30) {
+//            response = "You are really old"
+//        } else {
+//            response = "You are still young"
+//        }
+//    } else {
+//        response = "The value entered is not a number"
+//    }
+//    writeText(response)
+//}
+//document.getElementById("okButton").addEventListener("click", respondToUser);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
