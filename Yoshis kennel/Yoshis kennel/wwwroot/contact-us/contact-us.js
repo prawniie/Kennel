c6bf35ceb3cca9563130f3cde2481c16a4c34135
georgfalemark <georg@falemark.se>
2019-01-24 14:31:06 +0100

    contact us formulär 2.0

---
+++ Yoshis kennel/Yoshis kennel/wwwroot/contact-us/contact-us.js
@@ -0,0 +1,19 @@
+﻿
+function getId(id) {
+    return document.getElementById(id)
+}
+
+
+let formSelect = getId("FormControlSelect1");
+let message = getId("FormControlTextarea1");
+let preName = getId("InputPrename")
+let lastName = getId("InputLastname")
+let telephoneNumber = getId("InputTelephoneNumber")
+let email = getId("InputEmail")
+
+
+
+
+
+
+
